7114cdc7951b48f73f268f004636739582588587
Muhammad Yasir <m.yasir3497@gmail.com>
2023-09-15 01:10:54 +0500

    Application/Frontend - Configured React app - Created react app - Added redux and config -  Created initial structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Chat from './pages/chat/Index'
+import Dashboard from './pages/dashboard/Index'
+import { store } from './redux/store'
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+export default App
---
+++ frontend/src/redux/slices/chatSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const chatSlice = createSlice({
+  name: 'chat',
+  initialState: {},
+  reducers: {},
+})
+
+export const {} = chatSlice.actions
+export default chatSlice.reducer
