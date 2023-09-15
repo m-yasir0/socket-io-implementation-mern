c8f226e713475b9048c4483185a77261e986df53
Muhammad Yasir <m.yasir3497@gmail.com>
2023-09-15 19:55:38 +0500

    hotfix/Using IO before initialization

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const http = require('http')
-const socketIO = require('socket.io')
-const cors = require('cors')
-const errorHandler = require('./src/middleware/errorHandler')
-const indexRoutes = require(__dirname + '/src/routes/index.js')
-require(__dirname + '/src/sockets')(io)
+const express = require('express');
+const dotenv = require('dotenv');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
+const errorHandler = require('./src/middleware/errorHandler');
+const indexRoutes = require(__dirname + '/src/routes/index.js');
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const server = http.createServer(app)
-const io = socketIO(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+require(__dirname + '/src/sockets')(io);
 
-app.use(express.json())
-app.use(cors())
-app.use(indexRoutes)
-app.use(errorHandler)
+app.use(express.json());
+app.use(cors());
+app.use(indexRoutes);
+app.use(errorHandler);
 
-module.exports = server
+module.exports = server;
